1016e0be456f344084dc3d61c88f6ace3bbd0443
Stacey Zheng <staceyzheng10@gmail.com>
2022-08-13 21:54:15 -0400

    hidden button and id created

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../stylesheets/App.css';
 
 function Navbar() {
+    const [showLinks, setShowLinks] = useState(false);
     return (
         <nav className="navbar">
-            <div>
-                <div className='leftSide'>
-                    <div className='links'>
-                        <a href='/'>Home</a>
-                        <a href='/about'>About</a>
-                        <a href='/contact'>Contact</a>
-                    </div>
+            <div className='leftSide'>
+                <div className='links' id={showLinks ? "hidden" : ""}>
+                    <a href='/'>Home</a>
+                    <a href='/about'>About</a>
+                    <a href='/contact'>Contact</a>
                 </div>
+                <button onClick={() => setShowLinks(!showLinks)}>Open</button>
             </div>
-            <div>
-                <div className='rightSide'>
-                    <input type="text" placeholder="search"></input>
-                    <button className='search-button'>Search</button>
-                </div>
+            <div className='rightSide'>
+                <input type="text" placeholder="search"></input>
+                <button className='search-button'>Search</button>
             </div>
         </nav>
     );
