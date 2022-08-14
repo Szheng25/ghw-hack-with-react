0747e200c3aa35e37b924ff883e4147cd4bde35f
Stacey Zheng <staceyzheng10@gmail.com>
2022-08-14 01:50:19 -0400

    images and shadows added, more css

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,21 +1,32 @@
 import React, {useState} from 'react';
 import '../stylesheets/App.css';
+import bar from '../assets/hamburgerBar.png';
+import glass from '../assets/magnifyingGlass.png';
 
 function Navbar() {
     const [showLinks, setShowLinks] = useState(false);
+    
     return (
-        <nav className="navbar">
+        <nav className="Navbar">
             <div className='leftSide'>
                 <div className='links' id={showLinks ? "hidden" : ""}>
                     <a href='/'>Home</a>
                     <a href='/about'>About</a>
                     <a href='/contact'>Contact</a>
                 </div>
-                <button onClick={() => setShowLinks(!showLinks)}>Open</button>
+                <button onClick={() => setShowLinks(!showLinks)}>
+                        <div className='imgBar'>
+                            <img src={bar} alt="webpages"></img>
+                        </div>
+                </button>
             </div>
             <div className='rightSide'>
                 <input type="text" placeholder="search"></input>
-                <button className='search-button'>Search</button>
+                <button className='search-button'>
+                    <div className='imgGlass'>
+                        <img src={glass} alt="search"></img>
+                    </div>
+                </button>
             </div>
         </nav>
     );
